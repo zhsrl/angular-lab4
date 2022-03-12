31d3e6addb270366893b28e5f1cc518330d4a24c
zhsrl <thunderccloud@gmail.com>
2022-03-12 22:33:31 +0600

    Initial commit

---
+++ src/app/prodct-details/prodct-details.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { Product, products } from '../products';
+import { CartService } from '../cart.service';
+
+@Component({
+  selector: 'app-prodct-details',
+  templateUrl: './prodct-details.component.html',
+  styleUrls: ['./prodct-details.component.css'],
+})
+export class ProductDetailsComponent implements OnInit {
+  product: Product | undefined;
+
+  constructor(private route: ActivatedRoute, private cartService: CartService) {}
+
+  ngOnInit(): void {
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute = Number(routeParams.get('productId'));
+
+    this.product = products.find(
+      (product) => product.id === productIdFromRoute
+    );
+  }
+
+  addToCart(product: Product){
+    window.alert('This item has been added to the cart');
+    this.cartService.addToCart;
+  }
+}
