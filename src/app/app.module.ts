150ff84843457bbe05173d3a508d4fb8b1bff4e7
zhsrl <thunderccloud@gmail.com>
2022-03-12 23:47:56 +0600

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { CartComponent } from './cart/cart.component';
 
 import { HttpClientModule } from '@angular/common/http';
 import { ShippingComponent } from './shipping/shipping.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 @NgModule({
   imports: [
@@ -33,6 +34,7 @@ import { ShippingComponent } from './shipping/shipping.component';
     ProductDetailsComponent,
     CartComponent,
     ShippingComponent,
+    ProductItemComponent,
   ],
   bootstrap: [AppComponent],
 })
